be65da9b404c9d10fd8e7cb2e617f49897920a99
Dani El-Ayyass <dayyass@yandex.ru>
2022-05-05 22:17:18 +0300

    update parallel for

--- solutions/4_pi_loop.cpp
+++ solutions/4_pi_loop.cpp
@@ -12,15 +12,23 @@ int main() {
     double start_time = omp_get_wtime();
     omp_set_num_threads(NUM_THREADS);
 
-    #pragma omp parallel
-    {
-        double x, y;
-        #pragma omp for reduction (+:sum)
-        for(int i = 0; i < N_STEPS; i++) {
-            x = (i + 0.5) * step;
-            y = 4.0 / (1.0 + x*x);
-            sum += y;
-        }
+    // #pragma omp parallel
+    // {
+    //     double x, y;
+    //     #pragma omp for reduction (+:sum)
+    //     for(int i = 0; i < N_STEPS; i++) {
+    //         x = (i + 0.5) * step;
+    //         y = 4.0 / (1.0 + x*x);
+    //         sum += y;
+    //     }
+    // }
+
+    double x, y;
+    #pragma omp parallel for reduction (+:sum) private(x, y)
+    for(int i = 0; i < N_STEPS; i++) {
+        x = (i + 0.5) * step;
+        y = 4.0 / (1.0 + x*x);
+        sum += y;
     }
     
     double end_time = omp_get_wtime();
