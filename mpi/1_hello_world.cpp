b4a5be95b504d686f8f7fa87954cdadd25a96d1c
Dani El-Ayyass <dayyass@yandex.ru>
2022-05-09 14:02:37 +0300

    update mpi hello world program

--- mpi/1_hello_world.cpp
+++ mpi/1_hello_world.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 int main() {
-    int rank, size;
+    int rank, size, name_len;
 
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    printf("Hello World from process %i of %i process\n", rank, size);
-    
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    printf("Hello World from processor %s, rank %i of %i processors\n", processor_name, rank, size);
+
     MPI_Finalize();
     return 0;
 }
