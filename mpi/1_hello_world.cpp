210464180519871f2702697c117a24d0e8c2ea45
Dani El-Ayyass <dayyass@yandex.ru>
2022-05-10 20:10:20 +0300

    add mpi 2_pi_send_recv.cpp

--- mpi/1_hello_world.cpp
+++ mpi/1_hello_world.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 int main() {
-    int rank, size, name_len;
 
+    // Initialize the MPI environment
     MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
 
-    printf("Hello World from processor %s, rank %i of %i processors\n", processor_name, rank, size);
+    // Print off a hello world message
+    printf(
+        "Hello world from processor %s, rank %d out of %d processors\n",
+        processor_name, world_rank, world_size
+    );
 
+    // Finalize the MPI environment
     MPI_Finalize();
+
     return 0;
 }
---
+++ mpi/2_pi_send_recv.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cmath>
+#include <mpi.h>
+using namespace std;
+
+#define N_STEPS 100000000
+
+int main() {
+
+    MPI_Init(NULL, NULL);
+
+    int size, rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Only 2 processes required
+    if (size != 2) {
+        if (rank == 0) {
+            printf("Error: 2 processes required\n");
+            MPI_Abort(MPI_COMM_WORLD, MPI_ERR_OTHER); 
+        }
+    }
+
+    double x, y, my_sum = 0;
+    double step = 1.0 / (double)N_STEPS;
+
+    for(int i = rank; i < N_STEPS; i += size) {
+        x = (i + 0.5) * step;
+        y = 4.0 / (1.0 + x*x);
+        my_sum += y;
+    }
+
+    if (rank == 1) {
+        MPI_Send(&my_sum, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+    } else {
+        double other_sum = 0;
+        MPI_Status status;
+        MPI_Recv(&other_sum, 1, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD, &status);
+
+        double pi = (my_sum + other_sum) * step;
+        printf("Integral: %.17g\n", pi);
+        printf("Error: %.17g\n", abs(pi - M_PI));
+    }
+
+    MPI_Finalize();
+    return 0;
+}
